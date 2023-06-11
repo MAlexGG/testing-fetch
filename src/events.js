ce5917c4579f0741a5940c18361f456e424a4509
MAlexGG <alexandragalarzag@gmail.com>
2023-06-11 13:00:43 +0200

    add a fav start, functionality that get fill and get empty working

--- src/events.js
+++ src/events.js
@@ -1,7 +1,18 @@
 import { getApiData } from "./api.js";
-import { print } from "./print.js";
+import { appearStar, print, fillStar, emptyStar } from "./print.js";
 
 const button = document.getElementById('button');
 button.addEventListener('click', function (){
-print(getApiData());
+    print(getApiData());
+    appearStar();
+});
+
+const startStroke = document.getElementById('fav-stroke');
+startStroke.addEventListener('click', function(){
+    fillStar();
+});
+
+const startFill = document.getElementById('fav-fill');
+startFill.addEventListener('click', function(){
+    emptyStar();
 });
--- src/print.js
+++ src/print.js
@@ -1,4 +1,23 @@
 export async function print(data){
     const facts = document.getElementById('facts');
     facts.innerHTML = await data;
-} 
+}; 
+
+export function appearStar(){
+    const starStroke = document.getElementById('fav-stroke');
+    starStroke.style.display = "block";
+};
+
+export function fillStar(){
+    const starStroke = document.getElementById('fav-stroke');
+    starStroke.style.display = "none";
+    const starFill = document.getElementById('fav-fill');
+    starFill.style.display = "block";
+};
+
+export function emptyStar(){
+    const starStroke = document.getElementById('fav-stroke');
+    starStroke.style.display = "block";
+    const starFill = document.getElementById('fav-fill');
+    starFill.style.display = "none";
+};
